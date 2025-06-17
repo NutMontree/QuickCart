1f79c76c35c48bd378c3154568f6939e717a0d24
NutMontree <nutmontree29@gmail.com>
2025-06-17 08:13:29 +0700

    'v1'

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -8,10 +8,9 @@ import Product from "@/models/Product";
 
 //Config cloudinary
 cloudinary.config({
-    // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    // api_key: process.env.CLOUDINARY_API_KEY,
-    // api_secret: process.env.CLOUDINARY_API_SECRET,
-    cloudinary_url: process.env.CLOUDINARY_URL
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 export async function POST(request) {
